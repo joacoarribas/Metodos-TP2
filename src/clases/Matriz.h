f1cf8436a80bc77928497a7a1787157d90be7b96
joaquin <joacoarribas@hotmail.com>
2016-05-04 13:33:53 -0300

    Pareciera ser que KNN ya anda, junto con el input. Adjunto tests hechos a mano

--- src/clases/Matriz.h
+++ src/clases/Matriz.h
@@ -27,14 +27,14 @@ class Matriz {
 
     vector<double>& operator [] (int fila);
 
-    void etiquetar(int i, char etiqueta);
-    void estimar(int i, char etiqueta);
+    void etiquetar(int i, int etiqueta);
+    void estimar(int i, int etiqueta);
     void mostrar();
     Matriz& transponer();
     void randomizar(int semilla);
 
-    char dameEtiqueta(int i);
-    char dameEstimacion(int i);
+    int dameEtiqueta(int i);
+    int dameEstimacion(int i);
     int dimensionFilas();
     int dimensionColumnas();
 
@@ -46,8 +46,8 @@ class Matriz {
 
     int filas, columnas;
     vector< vector<double> > matriz;
-    vector<char> etiquetas;
-    vector<char> estimaciones;
+    vector<int> etiquetas;
+    vector<int> estimaciones;
 
 	/*******************************
 	 *          Funciones          *
