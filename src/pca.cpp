93635effeddb4ab99fde0c6da931f1ff74b554c7
Jésica <jesi@localhost.localdomain>
2016-05-06 11:28:38 -0300

    cambios mínimos a pca.cpp

--- src/pca.cpp
+++ src/pca.cpp
@@ -4,42 +4,48 @@
 #include <iostream>
 #include "metodoPotencia.cpp"
 
-vector<double>& calcular_media(Matriz& m) {
-  double n = (double)m.dimensionFilas();
-  vector<double> aux(m.dimensionFilas());
-  for (int i = 0; i < m.dimensionFilas(); ++i) {
-    for (int j = 0; j < m.dimensionColumnas(); ++j) {
-      if(j == m.dimensionColumnas() - 1){
-        aux[i] = (aux[i] + m[i][j]) / m.dimensionColumnas(); //no se si se pierde precisión así
+void calcular_media(Matriz& matriz, vector<double>& v) { //ver si se pierde precisión con la división
+  int n = matriz.dimensionFilas();
+  int m = matriz.dimensionColumnas();
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      if(j == m - 1){
+        v[i] = (v[i] + matriz[i][j]) / (float)m; //calculo promedio final
       } else {
-        aux[i] = (aux[i] + m[i][j]); //no se si se pierde precisión así
+        v[i] = (v[i] + matriz[i][j]); //acumulo
       } 
     }
   }
-  return aux;
 }
 
-void calcular_matriz(Matriz& m) {
-  vector<double> media = calcular_media(m);
-  double n = sqrt ((double)m.dimensionFilas() - 1.0);
-  for (int i = 0; i < m.dimensionFilas(); ++i) {
-    for (int j = 0; j < m.dimensionColumnas(); ++j) {
-      m[i][j] = (m[i][j] - media[i]) / n;
+void calcular_matriz(Matriz& matriz) {
+  int n = matriz.dimensionFilas();
+  int m = matriz.dimensionColumnas();
+
+  vector<double> media(n);
+  calcular_media(matriz, media);
+  double divisor = sqrt ((double)n - 1.0);
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      matriz[i][j] = (matriz[i][j] - media[i]) / divisor;
     }
   }
-  /* Cuando termina este paso tengo la matriz de covarianza */
-  //ahora tengo que diagonalizar m
-  Matriz *base_ortonormal = new Matriz(m.dimensionFilas(), m.dimensionFilas());
+  /* Cuando termina este paso tengo la matriz de covarianza en matriz */
+  //ahora tengo que diagonalizar matriz
+  Matriz *base_ortonormal = new Matriz(n, n);
   vector<double> aux(base_ortonormal->dimensionColumnas());  
   double autovalor;
-  for (int i = 0; i < m.dimensionColumnas(); ++i) {
+
+  for (int i = 0; i < m; ++i) {
     Matriz::cargarVector(aux);
-    autovalor = metodoPotencia(m, aux);
+    autovalor = metodoPotencia(matriz, aux); //calculo el i-ésimo autovalor
     for (int k = 0; k < base_ortonormal->dimensionColumnas(); ++k){
-      (*base_ortonormal)[i][k] = aux[k];
+      (*base_ortonormal)[i][k] = aux[k]; //completo matriz con base ortonormal
     }
+    /* Deflación */
     Matriz auxiliar = Matriz::multiplicarVectoresDameMatriz(aux, aux);
-    m - (auxiliar * autovalor);
+    matriz- (auxiliar * autovalor);
   } 
+  /* Tengo en matriz_ortonormal la matriz con base de autovectores de matriz */
   
 }
